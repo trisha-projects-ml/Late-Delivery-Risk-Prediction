68543b0902883f9c527ce5dcb6c78e59f56dc260
trisha-projects-ml <trishadutta099@gmail.com>
2026-05-09 22:20:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 # Load data
-df = pd.read_excel("APL_sample.csv")
+df = pd.read_csv("APL_sample.csv")
 
 # Drop columns
 df = df.drop([
